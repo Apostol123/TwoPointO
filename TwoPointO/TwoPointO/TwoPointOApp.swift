3d89d434b51b76d44352a7a8ad1c2313070b42c8
Apostol, Alexandru <alapostol@deloitte.es>
2022-10-06 22:19:10 +0200

    origin/main create project

---
+++ TwoPointO/TwoPointO/TwoPointOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoPointOApp.swift
+//  TwoPointO
+//
+//  Created by Apostol, Alexandru on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TwoPointOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
